2980fa581e055393aa71a92a34bd5d9dd57479c4
Jeremy Minton <jjm61@cam.ac.uk>
2015-08-05 06:25:34 +0100

    Problem 3: completed by reducing number by smallest factor

---
+++ problem3/problem3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""problem3.py: solution to Project Euler Problem 3. 'projecteuler.net/problem=3'
+
+What is the largest prime factor of the number 600851475143?"""
+
+___author___="Jeremy Minton"
+___status___="Passed"
+
+largestPrimeFactor = 1;
+factorTrial=2;
+#number=13195;
+number = 600851475143;
+while factorTrial<=number:
+    if number%factorTrial == 0:
+        largestPrimeFactor = factorTrial;
+        number = number/factorTrial;
+        print(largestPrimeFactor);
+    factorTrial += 1
